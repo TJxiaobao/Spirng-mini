63398373dfe5838a7dda0732c0189d313852a004
TJxiaobao <2922035952@qq.com>
2025-04-17 23:16:36 +0800

    add：start demo 8 start.

---
+++ Spring-mini-8/src/main/java/cn/bugstack/springframework/beans/factory/BeanFactoryAware.java
@@ -0,0 +1,8 @@
+package cn.bugstack.springframework.beans.factory;
+
+import cn.bugstack.springframework.beans.BeansException;
+
+public interface BeanFactoryAware extends Aware{
+
+    void setBeanFactory(BeanFactory beanFactory) throws BeansException;
+}
